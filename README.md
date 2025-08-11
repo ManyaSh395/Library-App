0a8df806187378a02208c1a04332e1df9535c6c7
Manya Sharma <manya.sharma395@gmail.com>
2025-08-11 21:48:40 +0530

    Add html, css, js and README files

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Library-App
+A simple JavaScript web app that lets you create, view, update, and remove books from a personal library — all in your browser.
+
+## Features
+Add New Books – Title, Author, Pages, and Read Status.
+
+Toggle Read Status – Mark a book as read/unread.
+
+Remove Books – Delete a book from the library.
+
+Unique IDs – Each book is assigned a unique crypto.randomUUID() so DOM and data stay linked.
+
+Data/UI Separation – All book data is stored in an array; UI is generated dynamically from that array.
+
+## Tech Stack
+HTML – Structure
+
+CSS – Styling
+
+JavaScript (Vanilla) – Functionality
---
+++ script.js
@@ -0,0 +1,90 @@
+const container = document.querySelector(".book-container");
+const dialog = document.querySelector("#bookFormDialog");
+const form = document.querySelector("#bookForm");
+
+
+const myLibrary = [];  //book objects are going to be stored in an array
+
+function Book(title, author, pages, read) {
+    this.id = crypto.randomUUID();
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addBookToLib(title, author, pages, read){
+    const book = new Book(title, author, pages, read);
+    myLibrary.push(book);
+    displayBooks();
+}
+
+Book.prototype.toggleRead = () => {
+    this.read = !this.read;
+};
+
+removeBook = (id) => {
+    const index = myLibrary.findIndex(book => book.id === id);
+    if(index != -1) {
+        myLibrary.splice(index, 1);
+        displayBooks();
+    }
+}
+
+toggleBookReadStatus = (id) => {
+    const book = myLibrary.find(book => book.id === id);
+    if(book) {
+        book.toggleRead();
+        displayBooks();
+    }
+}
+
+displayBooks = () => {
+    container.innerHTML = ''; //clear previous display
+
+    myLibrary.forEach(book => {
+        const card = document.createElement('div');
+        card.classList.add('book-card');
+        card.dataset.id = book.id; // .dataset → a special object that lets you set or read custom data-* attributes on HTML elements. When you later click “Remove” or “Toggle Read” on that card, you can read the data-id to figure out which Book object in myLibrary this card is linked to.
+        card.innerHTML = `
+            <h3>${book.title}</h3>
+            <p><strong>Author: </strong>${book.author}</p>
+            <p><strong>Pages:</strong> ${book.pages}</p>
+            <p><strong>Status:</strong> ${book.read ? '✅ Read' : '❌ Not Read'}</p>
+            <button class="toggleBtn">Toggle Read</button>
+            <button class="removeBtn">Remove</button>
+        `;  
+        
+        card.querySelector('.removeBtn').addEventListener("click", () => removeBook(book.id));
+        card.querySelector('.toggleBtn').addEventListener("click", () => toggleBookReadStatus(book.id));
+
+        container.appendChild(card);
+    });
+}
+
+newBook.addEventListener("click", (e) => dialog.showModal());
+closeFormBtn.addEventListener("click", (e) => dialog.close());
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const title = document.querySelector("#title").value;
+    const author = document.querySelector("#author").value;
+    const pages = parseInt(document.querySelector("#pages").value);
+    const read = document.querySelector("#read").checked;
+
+    addBookToLib(title, author, pages, read);
+    form.reset(); //reset the form fields after submission  
+    dialog.close();
+});
+
+
+addBookToLib('The Hobbit', 'J.R.R. Tolkien', 310, true);
+addBookToLib('1984', 'George Orwell', 328, false);
+
+
+
+
+
+
+
